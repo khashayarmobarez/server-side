0e27756397f05f46de103cbe0f6a9f0770905c6f
khashayar <khashayarmobarezbusiness@gmail.com>
2024-10-16 15:11:27 +0330

    learning ssg 2

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 # server side rendering
 
+## bonus topic
+
+### json-server
+
+how to make:
+1- npm i json-server
+2- add "serve-json": "json-server --watch db.json --port 3060" to you package.json
+3- make db.json files with your data base file
+
+how to run:
+1- npm run serve-json
+2- use the adress written obove you page of server for exp: http://localhost:3060/posts
+
 ## data fetching:
 
 data fetching can be done in serverside in nextjs,
--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,37 @@ import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// the data prop is gotten from the getStaticProps 
 export default function Home({data}) {
+
+  // the data is fetched in server side but here we can log it in client side(use it)
+  console.log(data)
+
   return (
     <div>
-       <h3>{data} server side notes</h3>
+       <h3>khashayar server side notes</h3>
+       {
+          data &&
+          data.map(post => 
+            <li key={post.id}>{post.title}</li>
+          )
+       }
     </div>
   )
 }
 
 // testing get data at ssg rendering
 export async function getStaticProps() {
-// fetched data from api
-  const data = 'khashayar mobarez'
+  // fetched data from api
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await res.json()
+
+  // this log shows on terminal because it happens in server
+  // console.log(data)
 
   return {
     props: {
-      data,
+      data
     }
   }  
 }
