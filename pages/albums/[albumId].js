d8991f36d8eba6d9ae79defaca51b2b7887a8754
khashayar <khashayarmobarezbusiness@gmail.com>
2024-10-29 13:19:22 +0330

    a bit commit

--- pages/albums/[albumId].js
+++ pages/albums/[albumId].js
@@ -17,6 +17,12 @@ export async function getServerSideProps(context) {
     const res = await fetch(`http://localhost:3060/albums/${params.albumId}`)
     const data = await res.json();
 
+    if(!data.title) {
+        return {
+            NotFound:true
+        }
+    }
+
     return {
         props: {album: data}
     }
